9bcc18e24ceeacbbe52c47f03049e52d48c651f5
Purogurama Z <73376015+ZumTheZazaKing@users.noreply.github.com>
2020-12-28 15:22:04 +0800

    Add files via upload

---
+++ Forest (Stage 1)/Go West/Into the hole/Explore the Mines/Go North/Run/Proceed/Pick up Lighter/Run Pass/Investigate the mines/Look at Watch/Ask/questions/get out/No/Proceed/script.js
@@ -0,0 +1,7 @@
+var myVar = setInterval(myTimer, 1000);
+var i = 2;
+
+function myTimer() {
+    document.getElementById("count").innerHTML = i;
+    i--;
+}
---
+++ Forest (Stage 1)/Go West/Into the hole/Explore the Mines/Go North/Run/Proceed/script.js
@@ -0,0 +1,7 @@
+var myVar = setInterval(myTimer, 1000);
+var i = 5;
+
+function myTimer() {
+document.getElementById("count").innerHTML = i;
+i--;
+}
